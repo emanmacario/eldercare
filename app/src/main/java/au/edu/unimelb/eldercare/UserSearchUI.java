57706d19b10d65f58a4507cb5aae44c0a5b3b5af
Alastair Paterson <Alastair.Paterson@reece.com.au>
2018-09-14 15:50:08 +1000

    Complete initial implementation of UI

--- app/src/main/java/au/edu/unimelb/eldercare/UserSearchUI.java
+++ app/src/main/java/au/edu/unimelb/eldercare/UserSearchUI.java
@@ -1,13 +1,24 @@
 package au.edu.unimelb.eldercare;
 
-import android.app.ListActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import au.edu.unimelb.eldercare.usersearch.SearchAdaptor;
 
-public class UserSearchUI extends ListActivity {
+public class UserSearchUI extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_search_ui);
+
+        RecyclerView rvSearch = findViewById(R.id.rvSearchResults);
+        SearchAdaptor adapter = new SearchAdaptor();
+
+        rvSearch.setAdapter(adapter);
+
+        // Set layout manager to position the items
+        rvSearch.setLayoutManager(new LinearLayoutManager(this));
     }
 }

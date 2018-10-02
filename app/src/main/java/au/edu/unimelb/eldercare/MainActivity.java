70f1deb7b9eae349e5d60384bb539cbca6faf500
J-Marshall1997 <jamesdm6@gmail.com>
2018-10-02 11:27:40 +1000

    refactoring

--- app/src/main/java/au/edu/unimelb/eldercare/MainActivity.java
+++ app/src/main/java/au/edu/unimelb/eldercare/MainActivity.java
@@ -6,11 +6,13 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.MenuItem;
 import android.widget.TextView;
 import au.edu.unimelb.eldercare.service.AuthenticationListener;
 import au.edu.unimelb.eldercare.service.AuthenticationService;
+import au.edu.unimelb.eldercare.user.SelectUserTypeActivity;
+import au.edu.unimelb.eldercare.user.User;
+
 import com.firebase.ui.auth.IdpResponse;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -18,8 +20,6 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-
-import java.util.concurrent.TimeUnit;
 
 import static au.edu.unimelb.eldercare.service.AuthenticationService.RC_SIGN_IN;
 

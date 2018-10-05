bba5cfd22dd62f1aacdce0d527abe866083101bf
emanmacario <emacario1997@hotmail.com>
2018-10-05 18:44:15 +1000

    Add simple voice call UI, which allows accept/decline calls

--- app/src/main/java/au/edu/unimelb/eldercare/VoiceCallActivity.java
+++ app/src/main/java/au/edu/unimelb/eldercare/VoiceCallActivity.java
@@ -4,9 +4,14 @@ import android.content.Intent;
 import android.media.AudioManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.sinch.android.rtc.PushPair;
@@ -25,7 +30,12 @@ public class VoiceCallActivity extends AppCompatActivity {
     private FirebaseUser mFirebaseUser;
     private SinchClient sinchClient;
     private Call call;
-    private Button button;
+    private Button callButton;
+    private Button acceptButton;
+    private Button declineButton;
+    private TextView callState;
+    private EditText recipientIdEditText;
+    private String recipientUserId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,13 +45,13 @@ public class VoiceCallActivity extends AppCompatActivity {
         // Get the current Firebase user
         mFirebaseUser = FirebaseAuth.getInstance().getCurrentUser();
 
-        // Set hardcoded recipient id for debugging
-        final String recipientUserId = "b";
+        // Set the call state text view
+        callState = (TextView) findViewById(R.id.call_state);
 
         // Instantiate a SinchClient using the SinchClientBuilder
         sinchClient = Sinch.getSinchClientBuilder()
                 .context(this)
-                .userId("a") // TODO: Replace id with mFirebaseUser.getUid();
+                .userId(mFirebaseUser.getUid()) // TODO: Replace id with mFirebaseUser.getUid();
                 .applicationKey("0a9ff560-c6ed-4d85-a4ab-1143c50eb1ae") // TODO: Don't commit keys
                 .applicationSecret("BqjIJOCIhEynByd5ApgSoA==")
                 .environmentHost("clientapi.sinch.com")
@@ -60,8 +70,8 @@ public class VoiceCallActivity extends AppCompatActivity {
         sinchClient.getCallClient().addCallClientListener(new SinchCallClientListener());
 
         // Set button properties
-        button = (Button) findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
+        callButton = (Button) findViewById(R.id.button);
+        callButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 // Make a call or hang up depending on whether or not the call is null
@@ -69,10 +79,67 @@ public class VoiceCallActivity extends AppCompatActivity {
                     CallClient callClient = sinchClient.getCallClient();
                     call = callClient.callUser(recipientUserId);
                     call.addCallListener(new SinchCallListener());
-                    button.setText("End Call");
+                    callButton.setText("End Call");
                 } else {
                     call.hangup();
                 }
+            }
+        });
+
+        // Set the accept and decline call buttons
+        acceptButton = (Button) findViewById(R.id.button_accept);
+        declineButton = (Button) findViewById(R.id.button_decline);
+
+        acceptButton.setVisibility(Button.GONE);
+        declineButton.setVisibility(Button.GONE);
+
+        acceptButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (call == null) {
+                    Log.d(TAG, "ERROR, ACCEPTING NULL CALL");
+                    return;
+                }
+                call.answer();
+                call.addCallListener(new SinchCallListener());
+                callButton.setText("End Call");
+                //acceptButton.setVisibility(Button.GONE);
+                //declineButton.setVisibility(Button.GONE);
+            }
+        });
+
+        declineButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (call != null) {
+                    call.hangup();
+                    acceptButton.setVisibility(Button.GONE);
+                    declineButton.setVisibility(Button.GONE);
+                }
+            }
+        });
+
+        recipientIdEditText = (EditText) findViewById(R.id.edittext_recipientid);
+        recipientIdEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                // Set the new recipient user id
+                recipientUserId = recipientIdEditText.getText().toString();
+
+                // Only enable call button if a string is entered
+                if (charSequence.toString().trim().length() > 0) {
+                    callButton.setEnabled(true);
+                } else {
+                    callButton.setEnabled(false);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
             }
         });
     }
@@ -84,7 +151,8 @@ public class VoiceCallActivity extends AppCompatActivity {
             // make volume buttons go back to controlling ringer volume
             Log.d(TAG, "onCallEnded called");
             call = null;
-            button.setText("Call");
+            callButton.setText("Call");
+            callState.setText("");
             setVolumeControlStream(AudioManager.USE_DEFAULT_STREAM_TYPE);
         }
 
@@ -93,6 +161,7 @@ public class VoiceCallActivity extends AppCompatActivity {
             // Incoming call was picked up. Make volume buttons
             // control the volume of the phone call while connected
             Log.d(TAG, "onCallEstablished called");
+            callState.setText("CONNECTED");
             setVolumeControlStream(AudioManager.STREAM_VOICE_CALL);
 
             // TODO: If progress tone is played, it should be stopped here
@@ -102,6 +171,8 @@ public class VoiceCallActivity extends AppCompatActivity {
         public void onCallProgressing(Call progressingCall) {
             // Call is currently being made (i.e. ringing)
             Log.d(TAG, "onCallProgressing called");
+            callState.setText("RINGING ...");
+
             // TODO: Add progress tone here to indicate outgoing call is being made
         }
 
@@ -115,22 +186,16 @@ public class VoiceCallActivity extends AppCompatActivity {
     private class SinchCallClientListener implements CallClientListener {
         @Override
         public void onIncomingCall(CallClient callClient, Call incomingCall) {
-            // Pick up the call
             Log.d(TAG, "onIncomingCall called");
 
             // TODO: Start playing ringtone
 
             call = incomingCall;
-            call.answer();
-            call.addCallListener(new SinchCallListener());
+            acceptButton.setVisibility(Button.VISIBLE);
+            declineButton.setVisibility(Button.VISIBLE);
+
 
             // TODO: Stop playing ringtone
-
-            button.setText("End Call");
-
-            Intent intent = new Intent(VoiceCallActivity.this, MessagingActivity.class);
-            startActivity(intent);
-
         }
     }
 }

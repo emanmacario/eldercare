33e568e2d008b10f3c803fd53773f61684ac589a
EmanMacario <emacario1997@hotmail.com>
2018-10-19 02:04:13 +1100

    Added on click functionality for buttons in slider menu

--- app/src/main/java/au/edu/unimelb/eldercare/HomeActivity.java
+++ app/src/main/java/au/edu/unimelb/eldercare/HomeActivity.java
@@ -5,23 +5,53 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import au.edu.unimelb.eldercare.event.EventsUI;
+import au.edu.unimelb.eldercare.service.AuthenticationService;
+import au.edu.unimelb.eldercare.user.SettingsUI;
+import au.edu.unimelb.eldercare.user.UserProfileUI;
 import au.edu.unimelb.eldercare.user.UserSearchUI;
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class HomeActivity extends AppCompatActivity {
-
-    private CircleImageView mButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        mButton = findViewById(R.id.image_message_profile);
+        // Create the Sinch Client for the current authenticated user
+        VoiceCallService sinchService = VoiceCallService.getInstance();
+        sinchService.buildSinchClient(this);
+    }
+
+    public void openFriends(View view) {
+        // TODO: Complete this and open friends list intent
+    }
+
+    public void openConnectedUser(View view) {
+        // TODO: Complete this and open connected user intent
+    }
+
+    public void openUserProfileUI(View view) {
+        String userId = AuthenticationService.getAuthenticationService().getUser().getUid();
+        Intent intent = new Intent(HomeActivity.this, UserProfileUI.class);
+        intent.putExtra("targetUser", userId);
+        startActivity(intent);
+    }
+
+    public void openSettings(View view){
+        Intent intent = new Intent(HomeActivity.this, SettingsUI.class);
+        startActivity(intent);
+    }
+
+    public void openEvents(View view) {
+        Intent intent = new Intent(HomeActivity.this, EventsUI.class);
+        startActivity(intent);
     }
 
     public void openUserSearch(View view) {
         Intent intent = new Intent(HomeActivity.this, UserSearchUI.class);
         startActivity(intent);
     }
+
 }
